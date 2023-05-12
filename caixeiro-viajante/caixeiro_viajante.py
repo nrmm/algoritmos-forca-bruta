661dcbbd8b4ea14fb1560cdbe8a02812d9cd903f
Nilander R. M. de Moraes <nrmm.dev@gmail.com>
2023-05-12 09:48:20 -0300

    Implementa algoritmo do caixeiro viajante

---
+++ caixeiro-viajante/caixeiro_viajante.py
@@ -0,0 +1,27 @@
+import math
+import itertools
+
+def encontre_ciclo_hamiltoniano_com_custo_minimo(grafo, vertice_inicial):
+    custo_minimo = math.inf
+    ciclo_com_custo_minimo = ()
+    n = grafo.numero_vertices()
+    conjunto_vertices = list(range(n))
+    # Remove vértice inicial, pois as permutações são geradas apenas
+    # para os vértices intermediários.
+    del conjunto_vertices[vertice_inicial]
+    for permutacao in itertools.permutations(conjunto_vertices, n - 1):
+        ciclo = (vertice_inicial,) + permutacao + (vertice_inicial,)
+        custo = 0
+        for i in range(len(ciclo) - 1):
+            u = ciclo[i]
+            v = ciclo[i + 1]
+            if grafo.adjacente(u, v):
+                custo += grafo.peso(u, v)
+            # Caso u-v não sejam adjacentes, não há ciclo.
+            else:
+                custo = math.inf
+                break
+        if custo < custo_minimo:
+            custo_minimo = custo
+            ciclo_com_custo_minimo = ciclo
+    return (ciclo_com_custo_minimo, custo_minimo)
